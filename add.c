cbf6e5562714a0fce3d6590e92eb6b754cf0029b
Jemima Thomas <156067274+Jemima1112@users.noreply.github.com>
2024-02-06 14:28:44 +0530

    Update add.c

--- add.c
+++ add.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2, diff;
+    int num1, num2, num3,diff;
     
     printf("Enter the first number: ");
     scanf("%d", &num1);
@@ -9,7 +9,7 @@ int main() {
     printf("Enter the second number: ");
     scanf("%d", &num2);
     
-    diff = num1 - num2;
+    diff = num3 - num2;
     
     printf("The diff of %d and %d is %d.", num1, num2, diff);
     
