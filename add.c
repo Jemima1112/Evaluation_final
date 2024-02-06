9604f64c1195248327d56041594a978bcd084242
Jemima Thomas <156067274+Jemima1112@users.noreply.github.com>
2024-02-06 13:59:34 +0530

    Update add.c

--- add.c
+++ add.c
@@ -9,9 +9,9 @@ int main() {
     printf("Enter the second number: ");
     scanf("%d", &num2);
     
-    sum = num1 + num2;
+    diff = num1 - num2;
     
-    printf("The sum of %d and %d is %d.", num1, num2, sum);
+    printf("The diff of %d and %d is %d.", num1, num2, diff);
     
     return 0;
 }
